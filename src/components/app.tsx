5c6a53f6562d5acabe22fed845453f03a6f90a11
Ted Shaffer <ted@brightsign.biz>
2019-09-06 08:28:20 -0700

    ongoing dev

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -7,14 +7,20 @@ import MenuItem from 'material-ui/MenuItem';
 
 class App extends React.Component<any, object> {
 
-  state = {
+  state: any = {
     mrCategory: 1,
+    restaurants: [
+      'La Costena',
+      'Pizza Chicago'
+    ],
+    currentRestaurantIndex: 0,
   };
 
   constructor(props: any) {
     super(props);
 
     this.handleMRCategoryChange = this.handleMRCategoryChange.bind(this);
+    this.handleRestaurantChange = this.handleRestaurantChange.bind(this);
   }
 
   handleMRCategoryChange(event: any, index: any, mrCategory: any) {
@@ -33,8 +39,39 @@ class App extends React.Component<any, object> {
           value={this.state.mrCategory}
           onChange={this.handleMRCategoryChange}
         >
-          <MenuItem value={1} primaryText='Food / Beverages' />
-          <MenuItem value={2} primaryText='Mtb Trails' />
+          <MenuItem key={1} value={1} primaryText='Food / Beverages' />
+          <MenuItem key={2} value={2} primaryText='Mtb Trails' />
+        </SelectField>
+      </div>
+    );
+  }
+
+  getRestaurants(): any {
+    const restaurants = this.state.restaurants.map((restaurantName: string, index: number) => {
+      return (
+        <MenuItem key={index} value={index} primaryText={restaurantName} />
+      );
+    });
+    return restaurants;
+  }
+
+  handleRestaurantChange(event: any, index: any, currentRestaurantIndex: any) {
+    this.setState(
+      {
+        currentRestaurantIndex,
+      }
+    );
+  }
+
+  renderRestaurants() {
+    return (
+      <div>
+        <SelectField
+          floatingLabelText='Restaurant'
+          value={this.state.currentRestaurantIndex}
+          onChange={this.handleRestaurantChange}
+        >
+          {this.getRestaurants()}
         </SelectField>
       </div>
     );
@@ -43,7 +80,10 @@ class App extends React.Component<any, object> {
   render() {
     return (
       <MuiThemeProvider>
-        {this.renderMRCategory()}
+        <div>
+          {this.renderMRCategory()}
+          {this.renderRestaurants()}
+        </div>
       </MuiThemeProvider>
     );
   }
