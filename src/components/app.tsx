f1908d9f5057e0f7c0ea29731a571a74a3c1af5c
Ted Shaffer <ted@brightsign.biz>
2019-09-08 07:26:56 -0700

    Pulled out the restaurant form as a separate component

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,13 +1,12 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 
+import Restaurant from './restaurant';
+
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 import RaisedButton from 'material-ui/RaisedButton';
-import TextField from 'material-ui/TextField';
-import Slider from 'material-ui/Slider';
-import Checkbox from 'material-ui/Checkbox';
 
 class App extends React.Component<any, object> {
 
@@ -18,14 +17,6 @@ class App extends React.Component<any, object> {
       'Pizza Chicago'
     ],
     currentRestaurantIndex: 0,
-    newRestaurantCategory: 1,
-    overallRestaurantRating: 5,
-    restaurantFoodRating: 5,
-    restaurantServiceRating: 5,
-    restaurantAmbienceRating: 5,
-    restaurantOutdoorSeating: false,
-    restaurantComments: '',
-    restaurantWouldVisitAgain: false,
   };
 
   style = {
@@ -38,16 +29,6 @@ class App extends React.Component<any, object> {
     this.handleMRCategoryChange = this.handleMRCategoryChange.bind(this);
     this.handleRestaurantChange = this.handleRestaurantChange.bind(this);
     this.handleAddRestaurant = this.handleAddRestaurant.bind(this);
-    this.handleRestaurantCategoryChange = this.handleRestaurantCategoryChange.bind(this);
-    this.handleRestaurantRatingChange = this.handleRestaurantRatingChange.bind(this);
-    this.handleRestaurantFoodRatingChange = this.handleRestaurantFoodRatingChange.bind(this);
-    this.handleSubmitAddNewRestaurant = this.handleSubmitAddNewRestaurant.bind(this);
-
-    this.handleRestaurantServiceRatingChange = this.handleRestaurantServiceRatingChange.bind(this);
-    this.handleRestaurantAmbienceRatingChange = this.handleRestaurantAmbienceRatingChange.bind(this);
-    this.handleRestaurantOutdoorSeatingChange = this.handleRestaurantOutdoorSeatingChange.bind(this);
-    this.handleRestaurantCommentsChange = this.handleRestaurantCommentsChange.bind(this);
-    this.handleRestaurantWouldVisitAgainChange = this.handleRestaurantWouldVisitAgainChange.bind(this);
   }
 
   handleMRCategoryChange(event: any, index: any, mrCategory: any) {
@@ -90,274 +71,6 @@ class App extends React.Component<any, object> {
     );
   }
 
-  renderRestaurantName() {
-    return (
-      <div>
-        <TextField
-          id='newRestaurantName'
-          hintText='Name'
-        /> <br />
-      </div>
-    );
-  }
-
-  renderRestaurantAddress() {
-    return (
-      <div>
-        <TextField
-          id='newRestaurantAddress'
-          hintText='Address'
-        /><br />
-      </div>
-    );
-  }
-
-  handleRestaurantCategoryChange(event: any, index: any, newRestaurantCategory: any) {
-    this.setState(
-      {
-        newRestaurantCategory,
-      }
-    );
-  }
-
-  renderRestaurantCategory() {
-    return (
-      <div>
-        <SelectField
-          floatingLabelText='Restaurant Category'
-          value={this.state.newRestaurantCategory}
-          onChange={this.handleRestaurantCategoryChange}
-        >
-          <MenuItem key={1} value={1} primaryText='Pizza' />
-          <MenuItem key={2} value={2} primaryText='Burritos' />
-          <MenuItem key={3} value={3} primaryText='Burgers' />
-        </SelectField>
-      </div>
-    );
-  }
-
-  handleRestaurantRatingChange(event: any, restaurantRating: any) {
-    console.log('restaurantRating: ' + restaurantRating);
-    this.setState(
-      {
-        restaurantRating,
-      }
-    );
-  }
-
-  renderOverallRestaurantRating() {
-    return (
-      <div>
-        <p>
-          <span>{'Overall rating: '}</span>
-          <span>{this.state.overallRestaurantRating}</span>
-          <span>{' on a scale of 0 to 10'}</span>
-        </p>
-        <Slider
-          min={0}
-          max={10}
-          step={0.1}
-          value={this.state.overallRestaurantRating}
-          onChange={this.handleRestaurantRatingChange}>
-        </Slider>
-      </div>
-    );
-  }
-
-  handleRestaurantFoodRatingChange(event: any, restaurantFoodRating: any) {
-    console.log('restaurantFoodRating: ' + restaurantFoodRating);
-    this.setState(
-      {
-        restaurantFoodRating,
-      }
-    );
-  }
-
-  renderFoodRating() {
-    return (
-      <div>
-        <p>
-          <span>{'Food rating: '}</span>
-          <span>{this.state.restaurantFoodRating}</span>
-          <span>{' on a scale of 0 to 10'}</span>
-        </p>
-        <Slider
-          min={0}
-          max={10}
-          step={0.1}
-          value={this.state.restaurantFoodRating}
-          onChange={this.handleRestaurantFoodRatingChange}>
-        </Slider>
-      </div>
-    );
-  }
-
-  handleRestaurantServiceRatingChange(event: any, restaurantServiceRating: any) {
-    console.log('restaurantRating: ' + restaurantServiceRating);
-    this.setState(
-      {
-        restaurantServiceRating,
-      }
-    );
-  }
-
-  renderServiceRating() {
-    return (
-      <div>
-        <p>
-          <span>{'Service rating: '}</span>
-          <span>{this.state.restaurantServiceRating}</span>
-          <span>{' on a scale of 0 to 10'}</span>
-        </p>
-        <Slider
-          min={0}
-          max={10}
-          step={0.1}
-          value={this.state.restaurantServiceRating}
-          onChange={this.handleRestaurantServiceRatingChange}>
-        </Slider>
-      </div>
-    );
-  }
-
-  handleRestaurantAmbienceRatingChange(event: any, restaurantAmbienceRating: any) {
-    console.log('restaurantAmbienceRating: ' + restaurantAmbienceRating);
-    this.setState(
-      {
-        restaurantAmbienceRating,
-      }
-    );
-  }
-
-  renderAmbienceRating() {
-    return (
-      <div>
-        <p>
-          <span>{'Ambience rating: '}</span>
-          <span>{this.state.restaurantAmbienceRating}</span>
-          <span>{' on a scale of 0 to 10'}</span>
-        </p>
-        <Slider
-          min={0}
-          max={10}
-          step={0.1}
-          value={this.state.restaurantAmbienceRating}
-          onChange={this.handleRestaurantAmbienceRatingChange}>
-        </Slider>
-      </div>
-    );
-  }
-
-  handleRestaurantOutdoorSeatingChange() {
-    console.log('restaurantOutdoorSeatingRating');
-    this.setState(
-      {
-        restaurantOutdoorSeating: !this.state.restaurantOutdoorSeating,
-      }
-    );
-  }
-
-  renderOutdoorSeating() {
-    return (
-      <div>
-        <Checkbox
-          label='Outdoor seating?'
-          checked={this.state.restaurantOutdoorSeating}
-          onCheck={this.handleRestaurantOutdoorSeatingChange.bind(this)}
-        />
-      </div>
-    );
-  }
-
-  renderRestaurantRatings() {
-    return (
-      <div>
-        Restaurant Ratings:
-        {this.renderOverallRestaurantRating()}
-        {this.renderFoodRating()}
-        {this.renderServiceRating()}
-        {this.renderAmbienceRating()}
-        {this.renderOutdoorSeating()}
-      </div>
-    );
-  }
-
-  handleRestaurantCommentsChange(event: any) {
-    this.setState(
-      {
-        restaurantComments: event.target.value
-      }
-    );
-  }
-
-  renderRestaurantComments() {
-    return (
-      <div>
-        Comments:
-        <br />
-        <TextField
-          id='restaurantComments'
-          hintText='Enter comments here....'
-          multiLine={true}
-          rows={4}
-          rowsMax={4}
-          value={this.state.restaurantComments}
-          onChange={this.handleRestaurantCommentsChange}
-        /><br />
-      </div>
-    );
-  }
-
-  handleRestaurantWouldVisitAgainChange() {
-    console.log('restaurantWouldVisitAgainRating');
-    this.setState(
-      {
-        restaurantWouldVisitAgain: !this.state.restaurantWouldVisitAgain,
-      }
-    );
-  }
-
-  renderWouldVisitAgain() {
-    return (
-      <div>
-        <Checkbox
-          label='Would visit again?'
-          checked={this.state.restaurantWouldVisitAgain}
-          onCheck={this.handleRestaurantWouldVisitAgainChange.bind(this)}
-        />
-      </div>
-    );
-  }
-
-  handleSubmitAddNewRestaurant() {
-    console.log('handleSubmitAddNewRestaurant');
-  }
-
-  renderSubmitAddNewRestaurant() {
-    return (
-      <div>
-        <RaisedButton
-          label='Submit'
-          onClick={this.handleSubmitAddNewRestaurant}
-        />
-      </div>
-    )
-
-  }
-  renderNewRestaurantForm() {
-    return (
-      <div>
-        {this.renderRestaurantName()}
-        {this.renderRestaurantAddress()}
-        {this.renderRestaurantCategory()}
-        {this.renderRestaurantRatings()}
-        {this.renderRestaurantComments()}
-        {this.renderWouldVisitAgain()}
-        {this.renderSubmitAddNewRestaurant()}
-      </div>
-    );
-  }
-
   handleAddRestaurant() {
     console.log('handleAddRestaurant invoked');
   }
@@ -378,7 +91,7 @@ class App extends React.Component<any, object> {
           onClick={this.handleAddRestaurant}
           style={{ display: 'inline-block' }}
         />
-        {this.renderNewRestaurantForm()}
+        <Restaurant/>
       </div>
     );
   }
