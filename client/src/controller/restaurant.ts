175b08930f2167faaf67f3e911c4935b58321f00
Ted Shaffer <ted@brightsign.biz>
2019-09-22 13:50:09 -0700

    add axios - start trying to use it.

--- client/src/controller/restaurant.ts
+++ client/src/controller/restaurant.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import {
   addRestaurant,
   updateRestaurantProperty,
@@ -13,7 +14,18 @@ export const addNewRestaurant = (restaurant: RestaurantDescription) => {
       visits: {},
       menuItems: {},
     };
-    dispatch(addRestaurant(restaurant.name, restaurantData)); 
+    // dispatch(addRestaurant(restaurant.name, restaurantData));
+
+    const path = 'http://localhost:8000/addRestaurant';
+    const data: any = { name: 'pizzaJoint' };
+    const promise = axios.post(path, data);
+    promise
+      .then( (response) => {
+        console.log('return from post');
+        console.log(response);
+      }).catch( (err: any) => {
+        console.log('err: ', err);
+      });
   };
 };
 
