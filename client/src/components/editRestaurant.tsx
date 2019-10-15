3ec2236c0e5ef1a1159a1d9348938d2c1d1b0fe7
Ted Shaffer <ted@brightsign.biz>
2019-10-15 12:49:07 -0700

    in progress....

--- client/src/components/editRestaurant.tsx
+++ client/src/components/editRestaurant.tsx
@@ -16,6 +16,7 @@ import {
   getRestaurantComments,
   getRestaurantWouldVisitAgain,
 } from '../selector';
+import { RestaurantSummary } from '../type';
 
 export interface EditRestaurantsProp {
   restaurantId: string;
@@ -28,18 +29,7 @@ export interface EditRestaurantsProp {
   restaurantOutdoorSeating: boolean;
   restaurantComments: string;
   restaurantWouldVisitAgain: boolean;
-  onSaveRestaurant: (
-    id: string,
-    name: string,
-    category: number,
-    overallRating: number,
-    foodRating: number,
-    serviceRating: number,
-    ambienceRating: number,
-    outdoorSeating: boolean,
-    comments: string,
-    wouldVisitAgain: boolean,
-  ) => any;
+  onSaveRestaurant: (restaurantSummary: RestaurantSummary) => any;
 }
 
 class EditRestaurantComponent extends React.Component<EditRestaurantsProp> {
@@ -47,23 +37,30 @@ class EditRestaurantComponent extends React.Component<EditRestaurantsProp> {
   constructor(props: EditRestaurantsProp) {
     super(props);
     this.handleOnSaveRestaurantEdits = this.handleOnSaveRestaurantEdits.bind(this);
+    this.handleOnCancelEditRestaurant = this.handleOnCancelEditRestaurant.bind(this);
+  }
+
+  handleOnCancelEditRestaurant() {
+    console.log('poo');
   }
 
   handleOnSaveRestaurantEdits() {
     console.log('handleOnSaveRestaurantEdits');
     console.log('invoke onSaveRestaurant');
-    this.props.onSaveRestaurant(
-      this.props.restaurantId,
-      this.props.restaurantName,
-      this.props.restaurantCategory,
-      this.props.restaurantOverallRating,
-      this.props.restaurantFoodRating,
-      this.props.restaurantServiceRating,
-      this.props.restaurantAmbienceRating,
-      this.props.restaurantOutdoorSeating,
-      this.props.restaurantComments,
-      this.props.restaurantWouldVisitAgain,
-    );
+
+    const restaurantSummary: RestaurantSummary = {
+      restaurantId: this.props.restaurantId,
+      name: this.props.restaurantName,
+      category: this.props.restaurantCategory,
+      overallRating: this.props.restaurantOverallRating,
+      foodRating: this.props.restaurantFoodRating,
+      serviceRating: this.props.restaurantServiceRating,
+      ambienceRating: this.props.restaurantAmbienceRating,
+      outdoorSeating: this.props.restaurantOutdoorSeating,
+      comments: this.props.restaurantComments,
+      wouldVisitAgain: this.props.restaurantWouldVisitAgain,
+    };
+    this.props.onSaveRestaurant(restaurantSummary);
   }
 
   render() {
@@ -71,6 +68,7 @@ class EditRestaurantComponent extends React.Component<EditRestaurantsProp> {
       <div>
         <RestaurantForm
           onSave={this.handleOnSaveRestaurantEdits}
+          onCancel={this.handleOnCancelEditRestaurant}
         />
       </div>
     );
@@ -81,7 +79,7 @@ function mapStateToProps(state: any) {
   return {
     restaurantId: getCurrentRestaurantId(state),
     restaurantName: getRestaurantName(state),
-    newRestaurantCategory: getRestaurantCategory(state),
+    restaurantCategory: getRestaurantCategory(state),
     restaurantOverallRating: getRestaurantOverallRating(state),
     restaurantFoodRating: getRestaurantFoodRating(state),
     restaurantServiceRating: getRestaurantServiceRating(state),
@@ -92,7 +90,7 @@ function mapStateToProps(state: any) {
   };
 }
 
-const mapDispatchToProps = (dispatch: any, ownProps: any) => {
+const mapDispatchToProps = (dispatch: any) => {
   return bindActionCreators({
     onSaveRestaurant: saveRestaurant,
   }, dispatch);
